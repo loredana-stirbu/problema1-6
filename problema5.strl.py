b3927e767a4b52e793fe494e2e2c9652545e21ad
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2020-09-22 19:03:40 +0300

    Add files via upload

---
+++ problema5.strl.py
@@ -0,0 +1,10 @@
+a=True
+b=False
+print("  a        b      a and b   b and a   a and a  b and b")
+print(a,    b,    a and b,      b and a,      a and a,       b and b, sep="     ")
+print()
+print("  a        b      a or b   b or a   a or a    b or b")
+print(a,    b,    a or b,      b or a,      a or a,       b or b, sep="     ")
+print()
+print("  a        b       not a     not b")
+print(a,        b,     not a,   not b, sep="     ")
